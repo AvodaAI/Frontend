1aaaf234dffeb09ddb3c7409c3f19bfc62f65756
AG <liamswift2014@gmail.com>
2024-12-06 00:28:19 -0500

    refactor: update layout components and styling for status and 404 pages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,17 @@
 // src/app/not-found.tsx
-import Link from "next/link"
-import { Button } from "./components/ui/button";
+'use server'
 
-export default function NotFound() {
+import Link from "next/link";
+import { Button } from "@components/ui/button";
+import { Section } from "@components/section";
+import { Container } from "@components/container";
+
+export default async function NotFound() {
   return (
-    <main className="min-h-screen flex flex-col justify-center items-center text-center">
+    <Container className="min-h-[80vh] flex flex-col justify-center items-center text-center">
+      <Section>
       <h1 className="text-[2.5rem] font-bold">
-        404 - Page Not Found!
+        Page Not Found!
       </h1>
       <p className="mt-2 text-xl text-gray-500">
         Sorry, the page you are looking for does not exist.
@@ -19,6 +24,7 @@ export default function NotFound() {
           Go Back Home
         </Button>
       </Link>
-    </main>
+      </Section>
+    </Container>
   );
 }
