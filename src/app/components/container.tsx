9e7697c10e9abf327aee1355b3fa5da4c518347c
AG <liamswift2014@gmail.com>
2024-12-08 21:22:09 -0500

    feat: add Container and Section layout components

---
+++ src/app/components/container.tsx
@@ -0,0 +1,27 @@
+//src/components/container.tsx
+import { cn } from "@/lib/utils"
+
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode
+  as?: keyof JSX.IntrinsicElements
+  className?: string
+}
+
+export function Container({
+  children,
+  as: Component = "div",
+  className,
+  ...props
+}: ContainerProps) {
+  return (
+    <Component 
+      className={cn(
+        "container mx-auto px-4 md:px-[--padding-md]",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Component>
+  )
+}
