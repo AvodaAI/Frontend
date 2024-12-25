99240fbe81aa63be2b82a200a306486109811f68
Sarika Bhimre <sarikabhimre18@gmail.com>
2024-12-25 17:06:35 +0530

    Displaying role and name on dashboard

--- src/app/(auth)/dashboard/page.tsx
+++ src/app/(auth)/dashboard/page.tsx
@@ -1,23 +1,12 @@
-import { cookies } from 'next/headers';
-import { supabase } from '@/utils/supabase/supabaseClient';
 import UserDetails from './components/userDetails';
 
-
-const getSession = async () => {
-  const cookieStore = await cookies()
-  const TOKEN = cookieStore.get('supabase-auth-token')
-  const user = await supabase.auth.getUser(TOKEN?.value);
-  return user
-}
-
 export default async function Page() {
-  const { data: { user } } = await getSession()
-
+ 
   return (
     <div className="container max-w-7xl mx-auto">
       <div className="flex flex-col gap-6">
         <h1 className="text-2xl font-bold tracking-tight">Dashboard</h1>
-        <UserDetails user={user} />
+        <UserDetails/>
       </div>
     </div>
   );
