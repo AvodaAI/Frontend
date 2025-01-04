6af6e61f7306d2bdf01cc9342a33d099a67c97ec
Sarika Bhimre <sarikabhimre18@gmail.com>
2025-01-04 15:05:50 +0530

    AV-70 removed unnecessary code and drizzle files

--- drizzle/0002_awesome_shotgun.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE "employees" CASCADE;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "clerk_id" varchar(255);--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "first_name" varchar(255);--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "last_name" varchar(255);--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "position" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "city" varchar(255);--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "country" varchar(255);--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "hire_date" timestamp;
