185c7b758e20068b43223eed3adacbad2d826a22
Sarika Bhimre <sarikabhimre18@gmail.com>
2025-01-15 16:49:18 +0530

    Closed AV-155

--- src/utils/timeFormatHandler.ts
+++ src/utils/timeFormatHandler.ts
@@ -33,3 +33,11 @@ export function formatTimeInHoursMinutes(seconds: number) {
 
   return result || "0m";
 }
+
+// Format time in HH:MM:SS
+export const formatTime = (seconds: number) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
+};
